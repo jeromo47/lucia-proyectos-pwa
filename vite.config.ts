75917b228ae2fbc29c829652276f3c67310fd32d
jeromo47 <jmolinacaparros@gmail.com>
2025-10-28 15:42:52 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -27,5 +28,10 @@ export default defineConfig({
         navigateFallback: '/index.html'
       }
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 });
